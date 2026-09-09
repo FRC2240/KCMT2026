28a22ebae66e1af76b969adfd187ea34aeddd388
exsfin <NRG8011@gmail.com>
2026-09-09 16:19:16 -0600

    Feeder implementation

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "KCMT2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "4405c270bda0d27cadb4f3e67bf7d33feb701334";
-  public static final String GIT_DATE = "2026-08-26 16:49:54 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-09-09 16:38:00 EDT";
-  public static final long BUILD_UNIX_TIME = 1788986280565L;
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "8d1f3ff98791860303c661e74b327f38402daa5a";
+  public static final String GIT_DATE = "2026-09-09 16:46:53 EDT";
+  public static final String GIT_BRANCH = "HEAD";
+  public static final String BUILD_DATE = "2026-09-12 12:23:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1789230224494L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

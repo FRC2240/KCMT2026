4466791631d2c9040ba6e799df0d1a8af6f26095
Laurens Schunk <laurensdschunk@gmail.com>
2026-09-19 13:04:48 -0600

    Fixes Part 1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.RobotBase;
+import frc.robot.subsystems.feeder.Feeder;
+import frc.robot.subsystems.feeder.FeederIOReal;
+import frc.robot.subsystems.feeder.FeederIOSim;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.drivetrain.DriveCommands;
 import frc.robot.subsystems.drivetrain.Drivetrain;
@@ -27,37 +30,40 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
 
+  private final Indexer indexer;
+  private final Feeder feeder;
+
   private final CommandXboxController controller = new CommandXboxController(0);
 
-  private final Indexer indexer;
+  private final Shooter shooter;
   private final Drivetrain drivetrain;
 
   public RobotContainer() {
-
- if (RobotBase.isReal()) {
+    if (RobotBase.isReal()) {
       indexer = new Indexer(new IndexerIOTalonFX());
       shooter = new Shooter(new ShooterIOTalonFX());
       drivetrain = new Drivetrain(
-        new ModuleIOTalonFX(TunerConstants.FrontLeft),
-        new ModuleIOTalonFX(TunerConstants.FrontRight),
-        new ModuleIOTalonFX(TunerConstants.BackLeft),
-        new ModuleIOTalonFX(TunerConstants.BackRight),
-        new GyroIOPigeon2()
-      );
+          new ModuleIOTalonFX(TunerConstants.FrontLeft),
+          new ModuleIOTalonFX(TunerConstants.FrontRight),
+          new ModuleIOTalonFX(TunerConstants.BackLeft),
+          new ModuleIOTalonFX(TunerConstants.BackRight),
+          new GyroIOPigeon2());
     } else {
       indexer = new Indexer(new IndexerIOSim());
       shooter = new Shooter(new ShooterIOSim());
+      feeder = new Feeder(new FeederIOSim());
       drivetrain = new Drivetrain(
-        new ModuleIO() {},
-        new ModuleIO() {},
-        new ModuleIO() {},
-        new ModuleIO() {},
-        new GyroIO() {}
-      );
+          new ModuleIO() {
+          },
+          new ModuleIO() {
+          },
+          new ModuleIO() {
+          },
+          new ModuleIO() {
+          },
+          new GyroIO() {
+          });
     }
-
-
-
 
     indexer.setDefaultCommand(indexer.enableCommand());
     drivetrain.setDefaultCommand(DriveCommands.controlDrivetrainWithController(drivetrain, controller));
